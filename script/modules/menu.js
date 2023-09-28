c63c03087f562d5981abb3fde810fbe1165bbb01
Denis Gorbunov <Gorbunosii@gmail.com>
2023-09-28 23:26:33 +0300

    ready

--- script/modules/menu.js
+++ script/modules/menu.js
@@ -2,23 +2,57 @@ const menu = document.querySelector(`.header-menu`);
 const overlayMenu = document.querySelector(`.overlay-menu`);
 const itemA = document.querySelectorAll(`.item-a`);
 
+let startTime = NaN;
+const durationOpacity = 300;
+
+const viseOverlay = (timestamp) => {
+  startTime ||= timestamp;
+  const progress = (timestamp - startTime) / durationOpacity;
+  console.log(progress);
+  overlayMenu.style.opacity = progress;
+
+  if (progress < 1) {
+    requestAnimationFrame(viseOverlay);
+  } else {
+    overlayMenu.style.opacity = 1;
+  }
+};
+
+const hideOverlay = (timestamp) => {
+  startTime ||= timestamp;
+  const progress = (startTime - timestamp) / durationOpacity;
+  console.log(progress);
+  overlayMenu.style.opacity = progress;
+
+  if (progress > 0) {
+    requestAnimationFrame(hideOverlay);
+  } else {
+    overlayMenu.style.opacity = 0;
+  }
+};
+
 menu.addEventListener(`click`, e => {
   const target = e.target;
   if (target === menu) {
-    menu.classList.toggle(`close-menu`);
-    overlayMenu.classList.toggle(`is-visible`);
+    if (menu.classList.contains(`close-menu`)) {
+      menu.classList.remove(`close-menu`);
+      requestAnimationFrame(hideOverlay);
+    } else {
+      menu.classList.add(`close-menu`);
+      requestAnimationFrame(viseOverlay);
+    }
   }
 });
 
 overlayMenu.addEventListener(`click`, () => {
   menu.classList.remove(`close-menu`);
-  overlayMenu.classList.remove(`is-visible`);
+  requestAnimationFrame(hideOverlay);
 });
 
 itemA.forEach(elem => {
   elem.addEventListener(`click`, () => {
     menu.classList.remove(`close-menu`);
-    overlayMenu.classList.remove(`is-visible`);
+    requestAnimationFrame(hideOverlay);
   });
 });
 
